c4579328f05f34ad8c6e03b4253f5334aa4d9aa7
Joerg Krause <joerg@krause.net>
2016-12-07 22:32:59 +0100

    Create lastindex.js

---
+++ lastindex.js
@@ -0,0 +1,12 @@
+var text = "Than Ann answers all questions again.";
+var patt = /a/g;
+var match = patt.exec(text);
+console.log(match[0] + " found at " + match.index);
+
+match = patt.exec(text);
+do {
+ match = patt.exec(text);
+ if (!match) break;
+ console.log(match[0] + " found at " + match.index);
+ console.log( "Further search at " + patt.lastIndex);
+} while(true);
